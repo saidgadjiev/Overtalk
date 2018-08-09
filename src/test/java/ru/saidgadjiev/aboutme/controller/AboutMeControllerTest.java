309846347d28d626af33ed14bcb5cefbc21eac4a
GadzhievSA <said.gadzhiev@id-mt.ru>
2018-08-09 23:22:07 +0300

    Тесты 1-я итерация

---
+++ src/test/java/ru/saidgadjiev/aboutme/controller/AboutMeControllerTest.java
@@ -0,0 +1,28 @@
+package ru.saidgadjiev.aboutme.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by said on 09.08.2018.
+ */
+@RunWith(SpringRunner.class)
+public class AboutMeControllerTest {
+
+    @TestConfiguration
+    static class OrmNextConfiguration {
+
+    }
+
+    @org.junit.Test
+    public void getAboutMe() throws Exception {
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+    }
+
+}
